f6e960370318c4d7f21cb6337c68c20205da40fb
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-06 15:14:01 +0200

    Arranged cosmos node table and variables

--- web-installer/src/data/substrate.js
+++ web-installer/src/data/substrate.js
@@ -17,7 +17,7 @@ export default {
     name: '',
     tendermintRPC: '',
     cosmosSDKRPC: '',
-    prometheusURL: '',
+    prometheusUrl: '',
     nodeExporterURL: '',
     isValidator: '',
     isArchive: '',
