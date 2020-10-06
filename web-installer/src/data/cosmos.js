168bda154b64e9f36d85ae149bb058646545dafe
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-06 14:30:27 +0200

    Data given to Kms form

--- web-installer/src/data/cosmos.js
+++ web-installer/src/data/cosmos.js
@@ -1,3 +1,5 @@
+import { CHANNELS_STEP, REPOSITORIES_STEP } from '../constants/constants';
+
 export default {
   cosmos: {
     title: 'Cosmos Chain Setup',
@@ -7,5 +9,17 @@ export default {
     description: 'We will now go through the setup of cosmos based chains.',
     placeholder: 'cosmos',
     tooltip: 'This will be used to identify the current chain that you are setting up.',
+  },
+  kmsForm: {
+    description: 'Kms description needs to be provided here.',
+    exporterUrlPlaceHolder: 'http://176.67.65.56:9100',
+    namePlaceHolder: 'Cosmos_KMS_Config_1',
+    nameTip: 'This will be used to identify the current KMS configuration.',
+    exporterUrlTip: 'This is your node exporter URL, if you visit the system '
+      + 'running KMS at the {IP_Addrees}:9100/metrics you should be able to'
+      + 'see a list of metrics in Prometheus format.',
+    monitorKmsTip: 'Enable if you want this KMS configuration to be monitored.',
+    backStep: REPOSITORIES_STEP,
+    nextStep: CHANNELS_STEP,
   },
 };
