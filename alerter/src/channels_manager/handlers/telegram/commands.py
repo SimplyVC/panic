f20662927b18d9afca5bb5c2c0afa24274a47ee1
Dylan Galea <dylan@simply-vc.com.mt>
2020-12-07 17:18:28 +0100

    Completed command handlers (pre testing). Now implementing the Telegram Commands Handler process.

--- alerter/src/channels_manager/handlers/telegram/commands.py
+++ alerter/src/channels_manager/handlers/telegram/commands.py
@@ -1,19 +1,48 @@
-from typing import Dict, List
+import logging
+from typing import Dict
 
-from telegram import Update
-from telegram.ext import CommandHandler, MessageHandler, Filters, \
-    CallbackContext
+from src.channels_manager.channels.telegram import TelegramChannel
+from src.channels_manager.commands.handlers.telegram_cmd_handlers import \
+    TelegramCommandHandlers
+from src.channels_manager.handlers.handler import ChannelHandler
+from src.data_store.mongo import MongoApi
+from src.data_store.redis import RedisApi
+from telegram.ext import CommandHandler, MessageHandler, Filters
 
-from src.utils.alert import Severity
 
+class TelegramCommandsHandler(ChannelHandler):
+    def __init__(self, logger: logging.Logger, handler_name: str,
+                 associated_chains: Dict, telegram_channel: TelegramChannel,
+                 redis: RedisApi, mongo: MongoApi) -> None:
+        super().__init__(handler_name, logger)
 
-class TelegramCommandsHandler:
-    def __init__(self, associated_chains: Dict) -> None:
-        self._associated_chains = associated_chains
+        self._cmd_handlers = TelegramCommandHandlers(
+            'Telegram Command Handlers', logger, redis, mongo,
+            associated_chains, telegram_channel)
+
+        command_specific_handlers = [
+            CommandHandler('start', self.cmd_handlers.start_callback),
+            CommandHandler('mute', self.cmd_handlers.mute_callback),
+            CommandHandler('unmute', self.cmd_handlers.unmute_callback),
+            CommandHandler('mute_all', self.cmd_handlers.mute_all_callback),
+            CommandHandler('unmute_all', self.cmd_handlers.unmute_all_callback),
+            CommandHandler('status', self.cmd_handlers.status_callback),
+            CommandHandler('ping', self.cmd_handlers.ping_callback),
+            CommandHandler('help', self.cmd_handlers.help_callback),
+            MessageHandler(Filters.command, self.cmd_handlers.unknown_callback)
+        ]
+
+        # TODO: Todo continue from TleemgraCommandHandler (from beginning) in
+        #     : panic_polakdot class.
+
+        # TODO: Must have it's own rabbit connection as this is in a separate
+        #     : thread and may block. This must also check that the updater
+        #     : thread is running and it must respond to heartbeats (similar to
+        #     : configs manager. If we do not manager to do this, block till we
+        #     : get an exit signal.
+
+        # TODO: Must also have graceful termination.
 
     @property
-    def associated_chains(self) -> Dict:
-        return self._associated_chains
-
-    def authorise(self, update: Update, context: CallbackContext) -> bool:
-        return True
+    def cmd_handlers(self) -> TelegramCommandHandlers:
+        return self._cmd_handlers
